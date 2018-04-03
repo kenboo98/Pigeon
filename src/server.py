64fc578f9a9092f2140630b56f957ce5e8af42c8
dandua98 <danishdua1998@gmail.com>
2018-04-03 01:37:36 -0600

    bootstrapped

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory, request, url_for
 from database import Database
-import passwords
+import password_manager
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def startup():
 
 @app.route("/")
 def home():
-    return app.send_static_file('../static/home.html')
+    return app.send_static_file('../static/index.html')
 
 @app.route("/login", methods = ['GET','POST'])
 def login():
@@ -18,23 +18,22 @@ def login():
         return app.send_static_file("../static/login.html")
     if request.method == 'POST':
         result = password_manager.verify_login(request.form['username'],request.form['password'])
-        if(result == "keyerror"):
-            return "KEYERROR"
-        if(result == True):
+        if (result == "KeyError"):
+            return "Error: Not a valid password form, please try again"
+        if (result == True):
             return "Welcome " + request.form['username']
         else:
-            return "Wrong Password or Username"
-
+            return "Error: Wrong Username or Password, please try again"
     else:
         return "error"
 
 @app.route("/new",methods = ['GET','POST'])
 def new_account():
     if request.method == 'GET':
-            return app.send_static_file("../static/new_account.html")
+            return app.send_static_file("../static/signup.html")
     if request.method == 'POST':
         if not request.form['password'] == request.form['confirm_password']:
-            return "Password Not Matching"
+            return "Error: Passwords did not match, please try again"
         else:
             password_manager.create_account(request.form['username'],request.form['password'])
             return "success"
