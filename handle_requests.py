aa350df243098a9af38d4c1e29edd93a6988c789
kenboo98 <tellambu@ualberta.ca>
2018-04-02 18:51:03 -0600

    Added password and username. Also password hashing

--- handle_requests.py
+++ handle_requests.py
@@ -1,23 +1,41 @@
 from flask import Flask, send_from_directory, request, url_for
 from database import Database
+import password_manager
 app = Flask(__name__)
 
 @app.before_first_request
 def startup():
-    global db
-    db = Database("message_list")
-    print("database tables created")
-
+    password_manager.initialize()
+    
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return app.send_static_file('home.html')
 
-@app.route("/comment_portal", methods = ['GET','POST'])
-def comment_portal():
+@app.route("/login", methods = ['GET','POST'])
+def login():
     if request.method == 'GET':
-        return app.send_static_file("index.html")
+        return app.send_static_file("login.html")
     if request.method == 'POST':
-        db.store_message(request.form['comment'])
-        return request.form['comment']
+        result = password_manager.verify_login(request.form['username'],request.form['password'])
+        if(result == "keyerror"):
+            return "KEYERROR"
+        if(result == True):
+            return "Welcome " + request.form['username']
+        else:
+            return "Wrong Password or Username"
+
     else:
         return "error"
+
+@app.route("/create_acc",methods = ['GET','POST'])
+def create_account():
+    if request.method == 'GET':
+            return app.send_static_file("create_account.html")
+    if request.method == 'POST':
+        if not request.form['password'] == request.form['confirm_password']:
+            return "Password Not Matching"
+        else:
+            password_manager.create_account(request.form['username'],request.form['password'])
+            return "success"
+    else:
+        return "Error"
